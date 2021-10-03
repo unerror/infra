eb858092baa2248c1351689cbcfb80c32e3bbf47
Dan Enman <enmand@gmail.com>
2021-10-03 17:38:33 -0300

    fix: remove argocd refresh annotation

--- argocd.tf
+++ argocd.tf
@@ -16,9 +16,6 @@ resource "argocd_repository" "infra-git" {
 
 resource "argocd_application" "argocd" {
   metadata {
-    annotations = {
-      "argocd.argoproj.io/refresh" = "normal"
-    }
     name      = "argocd"
     namespace = "default"
   }
