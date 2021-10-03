188e2039dc2affa108e66a90b8bb36733b1f5381
Dan Enman <enmand@gmail.com>
2021-10-03 18:28:12 -0300

    fix: rename tailscale app to networking

--- networking.tf
+++ networking.tf
@@ -6,7 +6,7 @@ resource "digitalocean_vpc" "kubernetes-tor1" {
 
 resource "argocd_application" "networking" {
   metadata {
-    name      = "tailscale"
+    name      = "networking"
     namespace = "default"
   }
 
