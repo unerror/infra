35143e124af536dd331e707e50d53e876851ba4c
Dan Enman <enmand@gmail.com>
2023-02-14 11:03:56 -0400

    chore: add db to k8s vpc

--- database.tf
+++ database.tf
@@ -1,10 +1,11 @@
 resource "digitalocean_database_cluster" "db-tor1" {
-  name       = "db-tor1"
-  engine     = "pg"
-  version    = var.postgres_version
-  size       = "db-s-1vcpu-1gb"
-  region     = var.do_region
-  node_count = 1
+  name                 = "db-tor1"
+  engine               = "pg"
+  version              = var.postgres_version
+  size                 = "db-s-1vcpu-1gb"
+  region               = var.do_region
+  node_count           = 1
+  private_network_uuid = digitalocean_vpc.kubernetes-tor1.id
 }
 
 resource "digitalocean_database_db" "dbs" {
