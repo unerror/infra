1ca028ef7066c9dce6a291468dc1834693154054
Dan Enman <enmand@gmail.com>
2024-07-15 23:09:30 -0300

    chore: disable coroot monitoring

--- monitoring.tf
+++ monitoring.tf
@@ -1,42 +1,42 @@
-resource "argocd_application" "monitoring" {
-  metadata {
-    name = "monitoring"
-  }
-
-  wait = true
-
-  spec {
-    project = argocd_project.infra.id
-
-    source {
-      repo_url        = var.infra_repo
-      path            = "charts/coroot"
-      target_revision = "HEAD"
-      helm {
-        value_files = ["values.yaml"]
-      }
-    }
-
-    sync_policy {
-      automated {
-        allow_empty = false
-        prune       = true
-        self_heal   = true
-      }
-    }
-
-    destination {
-      server    = "https://kubernetes.default.svc"
-      namespace = kubernetes_namespace.une-sys.id
-    }
-  }
-
-  depends_on = [
-    helm_release.argocd,
-    argocd_project.infra,
-    argocd_repository.infra-git
-  ]
-}
+# resource "argocd_application" "monitoring" {
+#   metadata {
+#     name = "monitoring"
+#   }
+#
+#   wait = true
+#
+#   spec {
+#     project = argocd_project.infra.id
+#
+#     source {
+#       repo_url        = var.infra_repo
+#       path            = "charts/coroot"
+#       target_revision = "HEAD"
+#       helm {
+#         value_files = ["values.yaml"]
+#       }
+#     }
+#
+#     sync_policy {
+#       automated {
+#         allow_empty = false
+#         prune       = true
+#         self_heal   = true
+#       }
+#     }
+#
+#     destination {
+#       server    = "https://kubernetes.default.svc"
+#       namespace = kubernetes_namespace.une-sys.id
+#     }
+#   }
+#
+#   depends_on = [
+#     helm_release.argocd,
+#     argocd_project.infra,
+#     argocd_repository.infra-git
+#   ]
+# }
 
 # resource "argocd_application" "monitoring" {
 #   metadata {
